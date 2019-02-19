0615dc10af545403ad3c19c1cb2981e380a9b4d6
Chris Gray <chris.gray@icsfl.com>
2019-02-19 00:23:42 -0500

    Updated unit tests

--- TogglDetailedApiHandler_Tests.py
+++ TogglDetailedApiHandler_Tests.py
@@ -9,35 +9,35 @@ class TogglDetailedApiHandler_Tests(unittest.TestCase):
         entries = [
             {
                 "description": "Meeting",
-                "start": "2004-02-04T09:15:41-05:00",
+                "start": "2004-02-28T09:15:41-05:00",
                 "dur": 557000,
                 "client": "client 1",
                 "project": "project 1"
             },
             {
                 "description": "Email",
-                "start": "2004-02-04T09:24:58-05:00",
+                "start": "2004-02-28T09:24:58-05:00",
                 "dur": 398000,
                 "client": "client 1",
                 "project": "project 1"
             },
             {
                 "description": "Sprint Launch",
-                "start": "2004-02-04T09:31:43-05:00",
+                "start": "2004-02-28T09:31:43-05:00",
                 "dur": 4755000,
                 "client": "client 1",
                 "project": "project 3"
             },
             {
                 "description": "Timesheets",
-                "start": "2004-02-04T10:50:45-05:00",
+                "start": "2004-02-28T10:50:45-05:00",
                 "dur": 2532000,
                 "client": "client 1",
                 "project": "project 1"
             },
             {
                 "description": "Customer Issue",
-                "start": "2004-02-04T11:32:58-05:00",
+                "start": "2004-02-28T11:32:58-05:00",
                 "dur": 339000,
                 "client": "client 1",
                 "project": "project 2"
@@ -46,11 +46,11 @@ class TogglDetailedApiHandler_Tests(unittest.TestCase):
 
         mappedEntries = TogglDetailedApiHandler(
             MockTogglDetailedApiReader(entries),
-            TogglDetailedApiMapper(DailyTimeEntryValidator())).handle("2004-02-04")
+            TogglDetailedApiMapper(DailyTimeEntryValidator())).handle("2004-02-28")
 
         self.assertEqual(len(mappedEntries), 5)
         self.assertEqual(mappedEntries[0].description, "Meeting")
-        self.assertEqual(mappedEntries[1].date, "2004-02-04")
+        self.assertEqual(mappedEntries[1].date, "2004-02-28")
         self.assertEqual(round(mappedEntries[2].duration, 3), 1.321)
         self.assertEqual(mappedEntries[3].client, "client 1")
         self.assertEqual(mappedEntries[4].project, "project 2")
@@ -59,4 +59,4 @@ class TogglDetailedApiHandler_Tests(unittest.TestCase):
         with self.assertRaises(Exception):
             TogglDetailedApiHandler(
                 MockTogglDetailedApiReader([]),
-                TogglDetailedApiMapper(DailyTimeEntryValidator())).handle("2004-02-04")
+                TogglDetailedApiMapper(DailyTimeEntryValidator())).handle("2004-02-28")
