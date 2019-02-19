b2852d82d3563275ca7696733156d06787abb6aa
Chris Gray <chris.gray@icsfl.com>
2019-02-18 19:33:24 -0500

    Trying formatting again

--- Run.bat
+++ Run.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 CD /d "%~dp0"
 
-SET ApiKey=
+SET ApiKey=3023c97c6b35259823d988083a5bc82e
 SET WorkspaceId=
-SET PythonPath=
+SET PythonPath=C:\Users\cgray\.virtualenvs\Timesheet-TfBeefbu\Scripts
 
 IF "%~1"=="" (
     GOTO DownloadData
