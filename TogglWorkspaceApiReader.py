54acc3044737b4790c69ae6a719a0c043c273857
Chris Gray <chris.gray@icsfl.com>
2019-02-23 11:03:18 -0500

    Added Toggl Workspace API methods for Clients, Projects, and Tasks

--- TogglWorkspaceApiReader.py
+++ TogglWorkspaceApiReader.py
@@ -11,3 +11,29 @@ class TogglWorkspaceApiReader:
             self.apiKey, 'api_token'))
 
         return response.json()
+
+    def getClients(self, workspaceId):
+        url = "https://www.toggl.com/api/v8/workspaces/{0}/clients".format(
+            workspaceId)
+
+        response = requests.get(url, auth=(
+            self.apiKey, 'api_token'))
+
+        return response.json()
+
+    def getProjects(self, workspaceId):
+        url = "https://www.toggl.com/api/v8/workspaces/{0}/projects".format(workspaceId)
+
+        response = requests.get(url, auth=(
+            self.apiKey, 'api_token'))
+
+        return response.json()
+
+    def getTasks(self, workspaceId):
+        url = "https://www.toggl.com/api/v8/workspaces/{0}/tasks".format(
+            workspaceId)
+
+        response = requests.get(url, auth=(
+            self.apiKey, 'api_token'))
+
+        return response.json()
